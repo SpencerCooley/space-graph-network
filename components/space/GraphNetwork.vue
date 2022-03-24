1e09de4d6856d1ec54287e11ab2ca14b08f91e77
Spencer Cooley <spencercooley@Spencers-Air.lan>
2022-03-24 14:41:33 -0700

    changed titles and byline

--- components/space/GraphNetwork.vue
+++ components/space/GraphNetwork.vue
@@ -5,10 +5,12 @@
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg>
       </span>
       <div class="header" :style="`background-image:url(${infoPanel.image});`">
-        <h1>{{infoPanel.title}}</h1><br>
-        <h2>By: {{infoPanel.artist}}</h2>
+        <!-- <h1>{{infoPanel.title}}</h1><br>
+        <h2>By: {{infoPanel.artist}}</h2> -->
       </div>
       <div class="content-body">
+        <h1>{{infoPanel.title}}</h1>
+         <h2>By: {{infoPanel.artist}}</h2>
         <p>{{infoPanel.description}}</p>
         <div class="themes">
           <span class="pill" v-for="theme in infoPanel.themes" :key="theme">{{theme}}</span>
@@ -269,7 +271,8 @@ export default {
 }
 .cta-btn {
   margin-top:15px;
-  background-color: #2081E2;
+  // background-color: #2081E2;
+  background-color: #ff2d55;
   border: none;
   color: white;
   padding: 15px 32px;
